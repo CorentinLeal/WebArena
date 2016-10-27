2c7730093e54f35a8a705efedeae551f34ef7046
Corentin Leal <leal.corentin@gmail.com>
2016-10-27 23:52:05 +0200

    Player.php | Creation of the model, add the function permiting to hash the passwords

---
+++ src/Model/Entity/Player.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: corentin
+ * Date: 27/10/16
+ * Time: 23:47
+ */
+
+namespace App\Model\Entity;
+
+use Cake\Auth\DefaultPasswordHasher;
+use Cake\ORM\Entity;
+
+class Player extends Entity
+{
+
+    protected $_accessible = [
+        '*' => true,
+        'id' => false
+    ];
+
+    protected function _setPassword($password){
+        return (new DefaultPasswordHasher)->hash($password);
+    }
+
+}
